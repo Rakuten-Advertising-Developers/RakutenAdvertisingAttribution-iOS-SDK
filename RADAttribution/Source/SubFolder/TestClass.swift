1fe63b3da96a40877afc65aa10c999f21dcd0532
Andrii Durbalo <ts-andrii.durbalo@rakuten.com>
2020-03-31 18:49:17 +0300

    Migrate to Swift 5 + subfolder

---
+++ RADAttribution/Source/SubFolder/TestClass.swift
@@ -0,0 +1,20 @@
+//
+//  TestClass.swift
+//  Pods
+//
+//  Created by Durbalo, Andrii on 31.03.2020.
+//
+
+import Foundation
+
+
+public class Lol {
+    
+
+    static public func looool() {
+        
+        print("loooooool")
+    }
+
+    
+}
