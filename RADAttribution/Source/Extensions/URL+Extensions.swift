ee4fe7444e9ffa04fe4ca41b39e5915f99ee56a9
Andrii Durbalo <ts-andrii.durbalo@rakuten.com>
2020-04-17 19:15:00 +0300

    Generate documentation script added to schemas, extend documentation a bit URL extension

--- RADAttribution/Source/Extensions/URL+Extensions.swift
+++ RADAttribution/Source/Extensions/URL+Extensions.swift
@@ -9,6 +9,16 @@ import Foundation
 
 extension URL: ExpressibleByStringLiteral {
     
+    /**
+     Initialize with string literal.
+     
+     - Parameter value: URL string value.
+     
+     - Returns: A new URL instanse by given parameter value.
+     
+     - Precondition: `value` should be valid for URL construction, otherwise, crash. Use it at your own risk.
+     */
+    
     public init(stringLiteral value: String) {
         self = URL(string: value)!
     }
