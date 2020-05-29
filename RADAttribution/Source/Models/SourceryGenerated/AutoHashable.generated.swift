a846298e2243954c07149745bfd47e08e898046a
Andrii Durbalo <ts-andrii.durbalo@rakuten.com>
2020-05-29 15:28:24 +0300

    MOB-71 Rename RADAttribution SDK to RakutenAdvertisingAttribution

--- RADAttribution/Source/Models/SourceryGenerated/AutoHashable.generated.swift
+++
@@ -1,9 +0,0 @@
-// Generated using Sourcery 0.18.0 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-
-// swiftlint:disable all
-
-
-// MARK: - AutoHashable for classes, protocols, structs
-
-// MARK: - AutoHashable for Enums
